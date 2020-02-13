be2720535fd575d6d2c7a671898e8409d1d75d74
AdamPhilipSmith <adamphilipsmith@hotmail.co.uk>
2020-02-13 11:16:38 +0000

    Before making more crazy changes

--- app/src/main/java/com/example/hygieneratingapp/MainActivity.java
+++ app/src/main/java/com/example/hygieneratingapp/MainActivity.java
@@ -106,64 +106,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //Enables new screen to display once a button is pushed
-    //public void addListenerOnButton() {
-        //final Context context = this;
-
-        //button = (Button) findViewById(R.id.button2);
-
-
-        //button.setOnClickListener(new View.OnClickListener() {
-
-            //@Override
-            //public void onClick(View arg0) {
-
-                //Intent intent = new Intent(context, MainActivity2.class);
-                //startActivity(intent);
-
-            //}
-
-        //});
-
-
-    //}
-
-
     public void Search(View v){
 
 
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        //TODO Maybe remove below
-        //Gets GPS Location
-        boolean ok2 = true;
-        for (int i = 0; i < requiredPermissions.length; i++) {
-            int result = ActivityCompat.checkSelfPermission(this,requiredPermissions[i]);
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                ok2 = false;
-            }
-        }
 
-        if (!ok2) {
-            ActivityCompat.requestPermissions(this, requiredPermissions, 1);
-            System.exit(0);
-        } else {
-            LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener(){
-                @Override
-                public void onLocationChanged(Location location){
-                    lat = location.getLatitude();
-                    lng = location.getLongitude();
-
-                }
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) { }
-                @Override
-                public void onProviderEnabled(String s){ }
-                @Override
-                public void onProviderDisabled(String s){ }
-            });
-        }
 
         //Connects to database using different search String depending on the button pushed.
         if (networkInfo !=null && networkInfo.isConnected()){
@@ -223,52 +171,30 @@ public class MainActivity extends AppCompatActivity {
                 }
                 in.close();
 
-
+                Intent sendText = new Intent(this, MainActivity2.class);
+                sendText.putExtra("t", responseBody);
+                //sendText.putExtra(Intent.EXTRA_STREAM, imageUri);
+                startActivity(sendText);
                 //Parses the String for the relevant information
-                LinearLayout layout = (LinearLayout)findViewById(R.id.Display4);
+                //LinearLayout layout = (LinearLayout)findViewById(R.id.Display4);
                 //imageUri = Uri.parse("android.resource://" + getPackageName()
                        // + "/drawable/" + "h0");
 
-                JSONArray aryJSONStrings = new JSONArray(responseBody);
-                String test3 = "";
-                for (int i=0; i<aryJSONStrings.length(); i++) {
-                    test3 += ("Address = ");
-                    test3 += aryJSONStrings.getJSONObject(i).getString("BusinessName");
-                    test3 += ("\nPost Code = ");
-                    test3 += aryJSONStrings.getJSONObject(i).getString("PostCode");
-                    test3 += ("\nRating = ");
 
-                    // Changes Rating to 'Exempt' if it is '-1'.
-                    if (aryJSONStrings.getJSONObject(i).getString("RatingValue").equals("-1")){
-                        test3 += "Exempt";
-                    }
-                    if (aryJSONStrings.getJSONObject(i).getString("RatingValue").equals("-1")){
-                        //layout.addView(image);
-
-                    }
-
-                    else test3 += aryJSONStrings.getJSONObject(i).getString("RatingValue");
-
-
-                    test3 += ("\n\n\n");
-                }
                 //String response = obj.getString("id");
                 //String test = "test";
 
                 //Sets intent so that the results can be displayed on the second page
                 //Intent SendBoth = new Intent(Intent.ACTION_SEND);
-                Intent sendText = new Intent(this, MainActivity2.class);
-                sendText.putExtra("t", test3);
+                //Intent sendText = new Intent(this, MainActivity2.class);
+                //sendText.putExtra("t", test3);
                 //sendText.putExtra(Intent.EXTRA_STREAM, imageUri);
-                startActivity(sendText);
+                //startActivity(sendText);
                 //input.setText(responseBody);
             }
             catch (IOException ioe){
                 input.setText("ERROR1");
                 ioe.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-                System.out.println("ERROR3");
             }
 
         } else {
--- app/src/main/java/com/example/hygieneratingapp/MainActivity2.java
+++ app/src/main/java/com/example/hygieneratingapp/MainActivity2.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 public class MainActivity2 extends Activity {
 
-
+    String test3 = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,50 @@ public class MainActivity2 extends Activity {
         //Bundle bundle = getIntent().getExtras();
         String message = startingIntent.getStringExtra("t");
 
+        JSONArray aryJSONStrings = null;
+        try {
+            aryJSONStrings = new JSONArray(message);
+
+
+        for (int i=0; i<aryJSONStrings.length(); i++) {
+            test3 += ("Address = ");
+            test3 += aryJSONStrings.getJSONObject(i).getString("BusinessName");
+            test3 += ("\nPost Code = ");
+            test3 += aryJSONStrings.getJSONObject(i).getString("PostCode");
+            test3 += ("\nRating = ");
+
+            // Changes Rating to 'Exempt' if it is '-1'.
+            if (aryJSONStrings.getJSONObject(i).getString("RatingValue").equals("-1")){
+                test3 += "Exempt";
+            }
+            if (aryJSONStrings.getJSONObject(i).getString("RatingValue").equals("-1")){
+                //layout.addView(image);
+
+            }
+
+            else test3 += aryJSONStrings.getJSONObject(i).getString("RatingValue");
+
+
+            test3 += ("\n\n\n");
+        }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
         TextView txtView = (TextView) findViewById(R.id.Display2);
-        txtView.setText(message);
+        txtView.setText(test3);
     }
 
 }
